2fc349ee8001382dc2674058f91ecc225f1de502
Babavali <valibab@gmail.com>
2025-05-22 18:15:56 +0530

    Update e_commerece.sql

--- e_commerece.sql
+++ e_commerece.sql
@@ -3,12 +3,12 @@
 DROP TABLE IF EXISTS `products`;
 CREATE TABLE `products` (
     `id` SERIAL PRIMARY KEY,
-    `platform_id` BIGINT UNSIGNED NOT NULL,
-    `product_reference` VARCHAR(50) UNIQUE NOT NULL,
-    `name` VARCHAR(100) NOT NULL,
-    `description` TEXT,
-    `price` DECIMAL(10,2) NOT NULL,
-    `status` VARCHAR(20) NOT NULL CHECK (status IN ('active', 'inactive', 'archived')),
+    `productReference` VARCHAR(50) UNIQUE NOT NULL,
+    `productName` VARCHAR(100) NOT NULL,
+    `productDescription` TEXT,
+    `currency` VARCHAR(100) NOT NULL,
+    `productPrice` DECIMAL(10,2) NOT NULL,
+    `productStatus` VARCHAR(20) NOT NULL CHECK (status IN ('active', 'inactive', 'archived')),
     `created_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (`platform_id`) REFERENCES `ecommerce_platforms`(`id`) ON DELETE CASCADE
@@ -18,10 +18,11 @@ CREATE TABLE `products` (
 DROP TABLE IF EXISTS `orders`;
 CREATE TABLE `orders` (
     `id` SERIAL PRIMARY KEY,
-    `platform_id` BIGINT UNSIGNED NOT NULL,
-    `order_reference` VARCHAR(50) UNIQUE NOT NULL,
-    `status` VARCHAR(20) NOT NULL CHECK (status IN ('pending', 'processing', 'shipped', 'delivered', 'cancelled', 'refunded')),
-    `amount` DECIMAL(10,2) NOT NULL,
+    `currency` VARCHAR(100) NOT NULL,
+    `platformType` VARCHAR(50) UNIQUE NOT NULL,
+    `orderReference` VARCHAR(50) UNIQUE NOT NULL,
+    `orderStatus` VARCHAR(20) NOT NULL CHECK (status IN ('pending', 'processing', 'shipped', 'delivered', 'cancelled', 'refunded')),
+    `orderAmount` DECIMAL(10,2) NOT NULL,
     `order_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     `customer_reference` VARCHAR(50) NOT NULL,
     `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
